ebbb846726709e11b6cd8532cbd1fa6de491a519
Rakesh Sahu <121565816+RakeshSahu24@users.noreply.github.com>
2024-07-17 14:20:19 +0530

    Create boilerPlate.cpp

---
+++ boilerPlate.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fo(i, n) for (i = 0; i < n; i++)
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+void solve() {
+    int N;
+    cin >> N;
+    vector<int> vec(N);
+    int i = 0;
+    fo(i, N) cin >> vec[i];
+
+
+}
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
