e04466264e559be928f30f621ebc8e4069300eb3
Rakesh Sahu <121565816+RakeshSahu24@users.noreply.github.com>
2024-07-21 13:28:08 +0530

    Update boilerPlate.cpp

--- boilerPlate.cpp
+++ boilerPlate.cpp
@@ -1,22 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define fo(i, n) for (i = 0; i < n; i++)
 #define ll long long
-#define pb push_back
-#define mp make_pair
-#define f first
-#define s second
-#define all(x) x.begin(), x.end()
-#define sortall(x) sort(all(x))
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
 #define PI 3.1415926535897932384626
 #define mod 1000000007
+
 void solve() {
-    int N;
-    cin >> N;
-    vector<int> vec(N);
-    int i = 0;
-    fo(i, N) cin >> vec[i];
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    
+    for (int i = 0; i < n; ++i)cin >> vec[i];
 
 
 }
